6922aa0aee6ebaeb7c18b2695c0ec1743b6c3d20
Rushabh Jhaveri <rjhaveri98@gmail.com>
2017-09-09 09:47:21 -0400

    Added Java Int to String.

---
+++ Introduction Challenges/Java Int to String/Int_to_String.java
@@ -0,0 +1,20 @@
+package hackerrank_java;
+
+import java.util.*;
+
+public class Int_to_String {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.close();
+		//String s = ???; Complete this line below
+		//Write your code here
+		String s = Integer.toString(n);
+		if(n == Integer.parseInt(s)){
+			System.out.println("Good job");
+		}
+		else{
+			System.out.println("Wrong answer");
+		}
+	}
+}
